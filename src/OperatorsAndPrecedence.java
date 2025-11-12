6f2e66acb86cdf542b709662fe0481c712f359a4
Wakoya Daba <waaqoyyaadhaabaa@gmail.com>
2025-11-13 00:07:15 +0300

    Update OperatorsAndPrecedence.java

--- src/OperatorsAndPrecedence.java
+++ src/OperatorsAndPrecedence.java
@@ -3,7 +3,6 @@ public class OperatorsAndPrecedence {
         int a = 10;
         int b = 20;
         int c = 30;
-
         // Arithmetic Operators
         System.out.println("a + b: " + (a + b));
         System.out.println("a - b: " + (a - b));
